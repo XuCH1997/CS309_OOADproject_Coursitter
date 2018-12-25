cf143450076c1ba079d0a9c82699e0de97a96244
Trevor Xu <10319343@qq.com>
2018-12-26 00:59:51 +0800

    123

--- db.py
+++ db.py
@@ -153,7 +153,7 @@ class conn():
     def withdraw_course(self, UID, CID, TERM="2018SPRING"):
         sql = "DELETE FROM STUDENT_COURSE WHERE SID = '{}' and CID = '{}' and TERM = '{}'".format(UID, CID, TERM)
         self.__execute_sql(sql)
-
+        self.commit_change()
 # if __name__ == '__main__':
 #     a = conn()
 #     a.select_course(UID=11510102, CID="CS004")
