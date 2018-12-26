1c19fddcc4c38d5aecf0831bcbb2626e55093a04
Trevor Xu <10319343@qq.com>
2018-12-26 16:17:58 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -171,7 +171,7 @@ def reco_list():
             msg = "Oops! It seems no recommendation here."
             final_list = None
         a.close()
-        return final_list
+        return jsonify(final_list)
     return redirect(url_for('login_page'))
 
 
